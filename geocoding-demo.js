6578d7d3e3b461250003e4c12a09038316780590
aolrich23 <andrea.olrich@gmail.com>
2013-11-27 15:58:28 +1100

    handleGeocodeResult function implemented

--- geocoding-demo.js
+++ geocoding-demo.js
@@ -1,10 +1,27 @@
 function handleGeocodeResult(latLng, address) {
   // TODO(olrichandrea): Write this function.
+  //move to spot
+  map.panTo(latLng);
   // - Place a marker on the map.
+  var marker = new google.maps.Marker({
+	  position: latLng,
+    map: map
+	});
+  
   // - Write something in the log.
+  log('Welcome to ' + address + '.');
+  
   // - Make an infowindow on the marker, displaying the address.
-
-  window.alert(address + ': ' + latLng);
+  var addressBox = '<div id="content">'+
+    address + '</div>';
+  
+  var infoWindow = new google.maps.InfoWindow({
+    position: latLng,
+    content: addressBox
+  });
+  infoWindow.open(map, marker);
+  
+  //window.alert(address + ': ' + latLng)
 }
 
 function log(msg) {
